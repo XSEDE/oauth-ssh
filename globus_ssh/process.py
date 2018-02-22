986b2edd074d04332d887fd623b766b054f3cf25
Jason Alt <jasonalt@globus.org>
2018-02-22 16:19:05 +0000

    Catch exceptions while running SCP/SSH and restore the terminal

--- globus_ssh/process.py
+++ globus_ssh/process.py
@@ -1,6 +1,8 @@
 import os
+import sys
 import pty
 import psutil
+import termios
 
 """Home for all process-related functions shared across other modules."""
 
@@ -12,7 +14,24 @@ def _get_process():
     parent = psutil.Process(os.getpid())
     return psutil.Process(parent.children()[0].pid)
 
+def _get_terminal_attrs():
+    """Return a list of the terminal attributes"""
+
+    return termios.tcgetattr(sys.stdin)
+
+def _set_terminal_attrs(atts):
+    """Set terminal attributes to the list provided"""
+    termios.tcsetattr(sys.stdin, termios.TCSANOW, atts)
+
 def spawn(argv, master_read):
     """spawn in a pseudo terminal and exit with its exit code when it completes"""
-    pty.spawn(argv, master_read)
-    return _get_process().wait()
+
+    atts = _get_terminal_attrs()
+
+    # Relying on the SSH process to have displayed the real error message
+    try:
+        pty.spawn(argv, master_read)
+        return _get_process().wait()
+    except:
+        _set_terminal_attrs(atts)
+        return 255
