4a4741c6083fc39618bb8d7b8fd569013f4b770d
ag012 <49694848+ag012@users.noreply.github.com>
2019-11-11 22:43:57 -0600

    Create scitoken_verify.h

---
+++ server/src/pam/scitoken_verify.h
@@ -0,0 +1,13 @@
+#ifndef _SVERIFY_H_
+#define _SVERIFY_H_
+
+/*
+ * System includes.
+ */
+#include <stdlib.h>
+#include <string.h>
+
+int
+scitoken_verify(const char * auth_line);
+
+#endif /* _SVERIFY_H_ */
