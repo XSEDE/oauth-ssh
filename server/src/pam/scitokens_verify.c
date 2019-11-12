9ac819a9e75dabfbb5b54d8a12bdd604f58ccfcd
AG <you@example.com>
2019-11-12 01:30:19 -0600

    config

--- server/src/pam/scitokens_verify.c
+++ server/src/pam/scitokens_verify.c
@@ -10,11 +10,12 @@
  */
 #include "scitokens.h"
 #include "logger.h"
-int scitoken_verify(const char * auth_line)
+#include "config.h"
+int scitoken_verify(const char * auth_line, const struct config * config)
 {
     SciToken scitoken;
     char *err_msg;
-    const char* listofauthz= "COPY:write DELETE:write GET:read HEAD:read LOCK:write MKCOL:write MOVE:write OPTIONS:read POST:read PROPFIND:write PROPPATCH:write PUT:write TRACE:read UNLOCK:write";
+    const char* listofauthz= "login:login COPY:write DELETE:write GET:read HEAD:read LOCK:write MKCOL:write MOVE:write OPTIONS:read POST:read PROPFIND:write PROPPATCH:write PUT:write TRACE:read UNLOCK:write";
 
     if(auth_line == NULL)
     {
@@ -27,8 +28,8 @@ int scitoken_verify(const char * auth_line)
     //This is the actual token
     auth_line = strtok(NULL, " ");
     while (*auth_line==' ') auth_line++;
-    int numberofissuer = 1;//TODO: Harcoded varible
-    char *null_ended_list[numberofissuer+1];
+    int numberofissuers = config->numberofissuers;
+    char *null_ended_list[numberofissuers+1];
 
     if (strcasecmp(auth_scheme, "Bearer"))
     {
@@ -38,9 +39,8 @@ int scitoken_verify(const char * auth_line)
     }
     
     //Read list of issuers from configuration and create a null ended list of strings
-    char *issuer = "https://demo.scitokens.org";//TODO: Harcoded varible
-    for(int i = 0; i<numberofissuer; i++) null_ended_list[i] = issuer;
-    null_ended_list[numberofissuer] = NULL;
+    for(int i = 0; i<numberofissuers; i++) null_ended_list[i] = config->issuers[i];
+    null_ended_list[numberofissuers] = NULL;
 
     if(sizeof(auth_line)>1000*1000)
     {
@@ -52,8 +52,8 @@ int scitoken_verify(const char * auth_line)
     if(scitoken_deserialize(auth_line, &scitoken, (const char * const*)null_ended_list, &err_msg))
     {
         logger(LOG_TYPE_INFO,
-        "Failed to deserialize scitoken %s \n %s",
-        auth_line,err_msg);
+        "Failed to deserialize scitoken %s \n %s + %s",
+	       auth_line,err_msg,config->issuers[0]);
         free(err_msg);
         return 0;
     }
@@ -97,7 +97,7 @@ int scitoken_verify(const char * auth_line)
     acl.resource = "";
     
     //Retrieve request type => acl.authz = read/write
-    char* requesttype = "GET";//TODO: Harcoded varible, in apache module, r->method;
+    char* requesttype = "login";//TODO: Add different type of permissions
     char* authzsubstr = strstr(listofauthz,requesttype);
     
     if(authzsubstr == NULL){
@@ -114,11 +114,24 @@ int scitoken_verify(const char * auth_line)
     //Resource is found/not found for the audience
     int found = 0;
     
-    for(int i=0; i<2; i++)//TODO: Harcoded varible 2, numberofissuer
+    for (int i = 0; config->scopes[i]; i++)
     {
-        //This for loop is suppose to search for the resource associated with the audience(in the config file)
-        acl.resource = "/path";//TODO: Harcoded varible
-        found = 1;
+        //search for the resource associated with the audience(in the config file)
+        if(!strncmp(config->scopes[i], requesttype, strlen(requesttype)))
+        {
+	    acl.resource = config->scopes[i] + strlen(requesttype) + 1;//skip :
+            found = 1;
+	    if (enforcer_test(enf, scitoken, &acl, &err_msg))
+            {
+	        logger(LOG_TYPE_INFO,
+                "Failed enforcer test %s %s %s %s",
+                 err_msg, acl.authz, acl.resource,aud_list[0]);
+                 free(err_msg);
+                 free(substr);
+                 return 0;
+            }
+            else goto success;
+        }
     }
     
     if(!found)
@@ -129,16 +142,8 @@ int scitoken_verify(const char * auth_line)
         return 0;
     }
 
-    if (enforcer_test(enf, scitoken, &acl, &err_msg))
-    {
-        logger(LOG_TYPE_INFO,
-        "Failed enforcer test %s %s %s %s",
-        err_msg, acl.authz, acl.resource,aud_list[0]);
-        free(err_msg);
-        free(substr);
-        return 0;
-    }
 
+    success:
     free(substr);
     return 1;
 }
