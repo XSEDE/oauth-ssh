980e2bc7d27133f9e1a083f625349f2efa39f60f
Jason Alt <jasonalt@globus.org>
2022-03-20 19:32:33 +0000

    Converted to nosetest to pytest

--- client/test/test_template.py
+++ client/test/test_template.py
@@ -1,85 +1,92 @@
+import pytest
+
 from oauth_ssh.template import Template
 
-def raises_error(F, *args, **kw):
-    try:
-        F(*args, **kw)
-    except:
-        return True
-    return False
+#
+# Initialization Tests
+#
+def test_initialize_no_template():
+    assert len(Template({}, {'A': 1}).keys()) == 0
 
-def illegal_insert(inst, key, value):
-    try:
-        inst()[key] = value
-    except:
-        return True
-    return False
+def test_initialize_no_values():
+    assert Template({'A': int},  {})['A'] is None
+    assert Template({'A': list}, {})['A'] is None
 
-class TestTemplate():
-    def test_creation_no_template(self):
-        assert len(Template({}, {'A': 1}).keys()) == 0
+def test_initialize_valid_values():
+    assert Template({'A': int},  {'A': 15})['A'] == 15
+    assert Template({'A': str},  {'A': '15'})['A'] == '15'
+    assert Template({'A': list}, {'A': ['15']})['A'] == ['15']
 
-    def test_creation_no_values(self):
-        assert Template({'A': int},  {})['A'] is None
-        assert Template({'A': list}, {})['A'] is None
+def test_initialize_valid_casts():
+    assert Template({'A': int},  {'A':    '15'})  ['A'] ==   15
+    assert Template({'A': list}, {'A':  "['15']"})['A'] == ['15']
 
-    def test_initial_valid_values(self):
-        assert Template({'A': int},  {'A': 15})['A'] == 15
-        assert Template({'A': str},  {'A': '15'})['A'] == '15'
-        assert Template({'A': list}, {'A': ['15']})['A'] == ['15']
+@pytest.mark.parametrize(
+    "template,values",
+    [
+        ({'A': list}, {'A': '[15]'}),
+        ({'A': list}, {'A': '15'}),
+        ({'A': list}, {'A': 15}),
+        ({'A': int},  {'A': '[15]'}),
+        ({'A': int},  {'A': 'B'}),
+        ({'A': str},  {'A': [15]}),
+        ({'A': str},  {'A': 15}),
+    ],
+)
+def test_initialize_invalid_casts(template, values):
+    with pytest.raises(TypeError):
+        Template(template, values)
 
-    def test_initial_valid_casts(self):
-        assert Template({'A': int},  {'A':    '15'})  ['A'] ==   15
-        assert Template({'A': list}, {'A':  "['15']"})['A'] == ['15']
+def test_initialize_invalid_key():
+    assert len(Template({}, {'B': 1})) == 0
 
-    def test_initial_invalid_casts(self):
-        assert raises_error(Template, {'A': list},  {'A': '[15]'})
-        assert raises_error(Template, {'A': list},  {'A':  '15'})
-        assert raises_error(Template, {'A': list},  {'A':   15})
+#
+# Insertion Tests
+#
 
-        assert raises_error(Template, {'A': int},  {'A': '[15]'})
-        assert raises_error(Template, {'A': int},  {'A':  'B'})
+def test_insertion():
+    template = Template({'A': int, 'B': str, 'C': list})
+    template['A'] =   15
+    template['B'] =  '15'
+    template['C'] = ['15']
+    assert template == {'A': 15, 'B': '15', 'C': ['15'] }
 
-        assert raises_error(Template, {'A': str},  {'A': [15]})
-        assert raises_error(Template, {'A': str},  {'A':  15})
+def test_insertion_with_casts():
+    template = Template({'I': int, 'L': list})
+    template['I'] =   '15'
+    template['L'] = "['15']"
+    assert template == {'I': 15, 'L': ['15'] }
 
-    def test_initial_invalid_key(self):
-        assert len(Template({}, {'B': 1})) == 0
+@pytest.mark.parametrize(
+    'template,key,value',
+    [
+        ({'A': list}, 'A', '[15]'),
+        ({'A': list}, 'A', '15'),
+        ({'A': list}, 'A', 15),
+        ({'A': int},  'A', '[15]'),
+        ({'A': int},  'A', 'B'),
+        ({'A': str},  'A', [15]),
+        ({'A': str},  'A', 15),
+    ]
+)
+def test_insertion_with_invalid_casts(template, key, value):
+    with pytest.raises(TypeError):
+        Template(template)[key] = value
 
-    def test_insertion(self):
-        template = Template({'A': int, 'B': str, 'C': list})
-        template['A'] =   15
-        template['B'] =  '15'
-        template['C'] = ['15']
-        assert template == {'A': 15, 'B': '15', 'C': ['15'] }
+def test_insertion_invalid_key():
+    with pytest.raises(KeyError):
+        Template({'A': list})['B'] = [15]
 
-    def test_insertion_with_casts(self):
-        template = Template({'I': int, 'L': list})
-        template['I'] =   '15'
-        template['L'] = "['15']"
-        assert template == {'I': 15, 'L': ['15'] }
+#
+# Deletion Tests
+#
 
+def test_deletion_int():
+    template = Template({'A': int},  {'A': 15})
+    del template['A']
+    assert template['A'] is None
 
-    def test_insertion_with_invalid_casts(self):
-        assert illegal_insert(Template({'A': list}), 'A', '[15]')
-        assert illegal_insert(Template({'A': list}), 'A',  '15')
-        assert illegal_insert(Template({'A': list}), 'A',   15)
-
-        assert illegal_insert(Template({'A': int}),  'A', '[15]')
-        assert illegal_insert(Template({'A': int}),  'A',  'B')
-
-        assert illegal_insert(Template({'A': str}),  'A', '[15]')
-        assert illegal_insert(Template({'A': str}),  'A',   15)
-
-    def test_insertion_invalid_key(self):
-        assert illegal_insert(Template({'A': list}), 'B', [15])
-
-    def test_deletion_int(self):
-        template = Template({'A': int},  {'A': 15})
-        del template['A']
-        assert template['A'] is None
-
-    def test_deletion_list(self):
-        template = Template({'A': list},  {'A': ['1']})
-        del template['A']
-        assert template['A'] is None
-
+def test_deletion_list():
+    template = Template({'A': list},  {'A': ['1']})
+    del template['A']
+    assert template['A'] is None
--- client/test/test_token.py
+++ client/test/test_token.py
@@ -1,5 +1,4 @@
 from oauth_ssh.token import Token
 
-class TestToken():
-    def test_expires_in(self):
-        assert 'expires_at' in Token(expires_in=10)
+def test_expires_in():
+    assert 'expires_at' in Token(expires_in=10)
