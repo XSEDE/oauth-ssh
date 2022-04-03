069550577836530adccea1503b593664b2ffbbbc
Jason Alt <jasonalt@globus.org>
2022-04-03 17:37:50 +0000

    dev release builds in client/ no longer modify setup.cfg

--- client/setup.py
+++ client/setup.py
@@ -1,4 +1,14 @@
+import configparser
+from os.path import exists
 from setuptools import setup
 
+
 if __name__ == "__main__":
-    setup()
+    # If the file 'clean' does not exist, then append '.dev0' to the package's
+    # version.
+    setup_args = {}
+    if not exists('clean'):
+        config = configparser.ConfigParser()
+        config.read('setup.cfg')
+        setup_args['version'] = config['metadata']['version'] + '.dev0'
+    setup(**setup_args)
